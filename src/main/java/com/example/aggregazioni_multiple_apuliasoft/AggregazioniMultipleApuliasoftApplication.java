c1471126e3ba669cb748d2564ea11cb854baac0a
alessio_cuoccio <alessio.cuoccio@outlook.com>
2023-06-07 16:19:55 +0200

    first commit

---
+++ src/main/java/com/example/aggregazioni_multiple_apuliasoft/AggregazioniMultipleApuliasoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.aggregazioni_multiple_apuliasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregazioniMultipleApuliasoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregazioniMultipleApuliasoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aggregazioni_multiple_apuliasoft/AggregazioniMultipleApuliasoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aggregazioni_multiple_apuliasoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregazioniMultipleApuliasoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
